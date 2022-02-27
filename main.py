9bbe03f63dcf4fecdce951a8e39bb94c6397860f
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-27 08:23:27 -0500

    add calculate and clear buttons

--- main.py
+++ main.py
@@ -23,3 +23,31 @@ w_label = Label(root, text='Mass', fg='red', bg='black', font=('Calibri', 14, 'b
 mas = Entry(root, textvariable=mass)
 w_label.grid(row=4)
 mas.grid(row=4, column=1)
+
+# BMI variable
+bmi_Val = DoubleVar()
+stat = StringVar()
+bmi = Label(root, text='BMI: ', fg='blue', bg='black', font=('Calibri', 14, 'bold'),
+            padx=2, pady=10, justify=LEFT)
+status = Label(root, text='Status', fg='blue', bg='black', font=('Calibri', 14, 'bold'),
+            padx=2, pady=10)
+status_msg = Label(root, textvariable=stat, fg='white', bg='black', font=('Calibri', 14, 'bold'),
+            padx=2, pady=10)
+BMI_total = Label(root, textvariable=bmi_Val, fg='white', bg='black', font=('Calibri', 14, 'bold'),
+            padx=2, pady=10)
+
+bmi.grid(row=6)
+status.grid(row=7)
+BMI_total.grid(row=6, column=1)
+status_msg.grid(row=7, column=1)
+
+# calc and clear buttons
+calculate = Button(root, text='Calculate', command=calc, fg='white', bg='black', font=('Calibri', 12, 'bold'))
+clear = Button(root, text='Reset', command=clear, fg='black', bg='white', font=('Calibri', 12, 'bold'))
+calculate.grid(row=8)
+clear.grid(row=8, column=3)
+
+
+
+
+root.mainloop()
