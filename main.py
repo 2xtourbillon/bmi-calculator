fa7cc3ccdc5d7e7d4c75699238329286bf3b04d5
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-27 08:15:51 -0500

    add mass variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tokenize import Double
 
 # root window
 root = Tk()
@@ -14,3 +15,11 @@ heigh = Entry(root, textvariable=height)
 h_label.grid(row=2)
 heigh.grid(row=2, column=1, columnspan=2, padx=5)
 
+# mass variable
+mass = DoubleVar()
+w_label = Label(root, text='Mass', fg='red', bg='black', font=('Calibri', 14, 'bold'),
+                 pady=10, padx=2)
+
+mas = Entry(root, textvariable=mass)
+w_label.grid(row=4)
+mas.grid(row=4, column=1)
