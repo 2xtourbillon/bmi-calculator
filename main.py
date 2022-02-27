bb468692c495ef5a023af83e7cfcde3d4688cdd4
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-27 08:13:02 -0500

    add height variable

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ root.title('BMI Calculator')
 root.configure(width=100, height=100)
 root.configure(bg='black')
 
-# variables
+# height label
 height = DoubleVar()
 h_label = Label(root, text='height', fg='red', bg='black', font=('Calibri', 14, 'bold'),
                  pady=5, padx=8)
 heigh = Entry(root, textvariable=height)
+h_label.grid(row=2)
+heigh.grid(row=2, column=1, columnspan=2, padx=5)
+
